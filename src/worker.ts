61c691e6f455249b18e55126bb9ba3efaa8330a8
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-06-29 02:56:23 +0300

    added concurrency

--- src/worker.ts
+++ src/worker.ts
@@ -3,18 +3,25 @@ import { Queue, QueueBaseOptions, Worker } from "bullmq";
 import config from "./config";
 import TranslationJob from "./jobs/translation";
 
+const { host, port, username, password } = config.redis;
+const concurrency = 200; // https://docs.bullmq.io/guide/workers/concurrency
+
 const opts: QueueBaseOptions = {
   connection: {
-    host: config.redis.host,
-    port: config.redis.port,
-    password: config.redis.password,
+    host,
+    port,
+    username,
+    password,
   },
   prefix: "vtrans",
 };
 
 export const translationQueue = new Queue("translation", opts);
 
-export const worker = new Worker("translation", TranslationJob.processor, opts)
+export const worker = new Worker("translation", TranslationJob.processor, {
+  ...opts,
+  concurrency,
+})
   .on("completed", TranslationJob.onCompleted)
   .on("progress", TranslationJob.onProgress)
   .on("failed", TranslationJob.onFailed);
