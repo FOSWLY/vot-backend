ea96b8688ff9b4a1c189161cfecd5aaa7eea5eb3
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-06-29 02:49:25 +0300

    updated services, added rawVideo validation

--- src/models/translation.model.ts
+++ src/models/translation.model.ts
@@ -5,11 +5,17 @@ const ToLang = t.TemplateLiteral("${ru|en|kk}");
 
 export const videoTranslationModels = new Elysia().model({
   "video-translation.translate": t.Object({
-    service: t.TemplateLiteral("${patreon|reddit}"),
+    service: t.TemplateLiteral("${mux|reddit|kodik}"),
     videoId: t.Union([t.String(), t.Number()]),
     fromLang: FromLang,
     toLang: ToLang,
-    rawVideo: t.Optional(t.String()),
+    rawVideo: t.Optional(
+      t.String({
+        format: "uri",
+        pattern: "^https://*",
+        minLength: 12,
+      }),
+    ),
     provider: t.Literal("yandex"),
   }),
 });
