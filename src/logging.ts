d1b8f7af2290754f6deb48ba548837a087f00906
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-07-05 16:20:10 +0300

    fixed app label

--- src/logging.ts
+++ src/logging.ts
@@ -48,7 +48,7 @@ if (loki.host) {
     options: {
       batching: true,
       interval: 5,
-      labels: { application: "media-converter-backend" },
+      labels: { application: "vot-backend" },
       host: loki.host,
       basicAuth:
         loki.user && loki.password
