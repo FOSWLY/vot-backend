f21794f1a275b9b3d038c3b0b7616b51777d5d00
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-21 13:31:18 +0300

    feat: add config validation schema

--- src/config.ts
+++ src/config.ts
@@ -1,79 +1,61 @@
 import * as path from "node:path";
-import { type Level } from "pino";
 
-import { version } from "../package.json";
+import { Value } from "@sinclair/typebox/value";
 
-export default {
+import { ConfigSchema } from "@/schemas/config";
+
+export default Value.Parse(ConfigSchema, {
   server: {
-    port: Bun.env.SERVICE_PORT ?? 3001,
-    hostname: Bun.env.SERVICE_HOST ?? "0.0.0.0",
+    port: Bun.env.SERVICE_PORT,
+    hostname: Bun.env.SERVICE_HOST,
   },
   app: {
-    name: Bun.env.APP_NAME ?? "[FOSWLY] VOT Backend",
-    desc: Bun.env.APP_DESC ?? "",
-    version,
-    license: "MIT",
-    github_url: "https://github.com/FOSWLY/vot-backend",
-    contact_email: Bun.env.APP_CONTACT_EMAIL ?? "me@toil.cc",
-    scalarCDN: "https://unpkg.com/@scalar/api-reference@1.15.1/dist/browser/standalone.js",
+    name: Bun.env.APP_NAME,
+    desc: Bun.env.APP_DESC,
+    contact_email: Bun.env.APP_CONTACT_EMAIL,
   },
-  cors: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-    "Access-Control-Max-Age": "86400",
-  },
+  cors: {},
   logging: {
-    level: (Bun.env.NODE_ENV === "production" ? "info" : "debug") as Level,
+    level: Bun.env.NODE_ENV === "production" ? "info" : "debug",
     logPath: path.join(__dirname, "..", "logs"),
     loki: {
-      host: Bun.env.LOKI_HOST ?? "",
-      user: Bun.env.LOKI_USER ?? "",
-      password: Bun.env.LOKI_PASSWORD ?? "",
-      label: Bun.env.LOKI_LABEL ?? "vot-backend",
+      host: Bun.env.LOKI_HOST,
+      user: Bun.env.LOKI_USER,
+      password: Bun.env.LOKI_PASSWORD,
+      label: Bun.env.LOKI_LABEL,
     },
   },
   services: {
     mediaConverter: {
-      hostname: Bun.env.MEDIA_CONVERTER_HOSTNAME ?? "http://127.0.0.1:3001",
-      token: Bun.env.MEDIA_CONVERTER_TOKEN ?? "",
-      maxReqInterrupt: 120, // if the number of request repeats > the maxRequestInterrupt, then abort as a failure
-      convertReqInterval: 2500, // 24 req/min for single convert
+      hostname: Bun.env.MEDIA_CONVERTER_HOSTNAME,
+      token: Bun.env.MEDIA_CONVERTER_TOKEN,
     },
     translateText: {
-      hostname: Bun.env.TRANSLATE_TEXT_HOSTNAME ?? "http://127.0.0.1:3313",
+      hostname: Bun.env.TRANSLATE_TEXT_HOSTNAME,
     },
   },
   db: {
-    name: Bun.env.POSTGRES_NAME ?? "vot-backend",
-    host: Bun.env.POSTGRES_HOST ?? "127.0.0.1",
-    port: Bun.env.POSTGRES_PORT ?? 5432,
-    user: Bun.env.POSTGRES_USER ?? "postgres",
-    password: Bun.env.POSTGRES_PASSWORD ?? "postgres",
-    outdateAfter: 604_800_000, // in ms
+    name: Bun.env.POSTGRES_NAME,
+    host: Bun.env.POSTGRES_HOST,
+    port: Bun.env.POSTGRES_PORT,
+    user: Bun.env.POSTGRES_USER,
+    password: Bun.env.POSTGRES_PASSWORD,
   },
   redis: {
-    host: Bun.env.REDIS_HOST ?? "127.0.0.1",
-    port: Bun.env.REDIS_PORT ?? 6379,
-    username: Bun.env.REDIS_USER ?? "default",
-    password: Bun.env.REDIS_PASSWORD ?? "",
-    prefix: Bun.env.REDIS_PREFIX ?? "votb", // Only for DB caching. BullMQ uses other prefix!
-    ttl: Bun.env.REDIS_TTL ?? 7200, // Only for DB caching. BullMQ uses own impl
+    host: Bun.env.REDIS_HOST,
+    port: Bun.env.REDIS_PORT,
+    username: Bun.env.REDIS_USER,
+    password: Bun.env.REDIS_PASSWORD,
+    prefix: Bun.env.REDIS_PREFIX,
+    ttl: Bun.env.REDIS_TTL,
   },
   s3: {
-    region: Bun.env.S3_REGION ?? "ru-central1", // if you use other s3 provider change this region
-    endpoint: Bun.env.S3_ENDPOINT ?? "https://storage.yandexcloud.net",
-    bucket: Bun.env.S3_BUCKET ?? "example",
-    accessKeyID: Bun.env.S3_ACCESS_KEY_ID ?? "",
-    secretAccessKey: Bun.env.S3_SECRET_ACCESS_KEY ?? "",
+    region: Bun.env.S3_REGION,
+    endpoint: Bun.env.S3_ENDPOINT,
+    bucket: Bun.env.S3_BUCKET,
+    accessKeyID: Bun.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: Bun.env.S3_SECRET_ACCESS_KEY,
   },
-  navigation: {
-    defaultLimit: Number(Bun.env.NAVIGATION_DEFAULT_LIMIT ?? 10),
-    maxLimit: Number(Bun.env.NAVIGATION_MAX_LIMIT ?? 50),
-    maxPage: 2147483647,
-  },
-  downloaders: {
-    userAgent:
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36",
-  },
-};
+  navigation: {},
+  downloaders: {},
+});
---
+++ src/schemas/config.ts
@@ -0,0 +1,101 @@
+import { Type as t, type Static } from "@sinclair/typebox";
+
+import { version } from "../../package.json";
+
+export const LoggingLevel = t.Union(
+  [
+    t.Literal("info"),
+    t.Literal("debug"),
+    t.Literal("fatal"),
+    t.Literal("error"),
+    t.Literal("warn"),
+    t.Literal("trace"),
+  ],
+  {
+    default: "info",
+  },
+);
+
+const license = "MIT";
+const scalarCDN = "https://unpkg.com/@scalar/api-reference@1.25.64/dist/browser/standalone.js";
+
+export const ConfigSchema = t.Object({
+  server: t.Object({
+    port: t.Number({ default: 3001 }),
+    hostname: t.String({ default: "0.0.0.0" }),
+  }),
+  app: t.Object({
+    name: t.String({ default: "[FOSWLY] VOT Backend" }),
+    desc: t.String({ default: "" }),
+    version: t.Literal(version, { readOnly: true, default: version }),
+    license: t.Literal(license, { readOnly: true, default: license }),
+    github_url: t.String({
+      default: "https://github.com/FOSWLY/vot-backend",
+    }),
+    contact_email: t.String({ default: "me@toil.cc" }),
+    scalarCDN: t.Literal(scalarCDN, { readOnly: true, default: scalarCDN }),
+  }),
+  cors: t.Object({
+    "Access-Control-Allow-Origin": t.String({ default: "*" }),
+    "Access-Control-Allow-Headers": t.String({ default: "*" }),
+    "Access-Control-Allow-Methods": t.String({ default: "POST, GET, DELETE, OPTIONS" }),
+    "Access-Control-Max-Age": t.String({ default: "86400" }),
+  }),
+  logging: t.Object({
+    level: LoggingLevel,
+    logPath: t.String(),
+    loki: t.Object({
+      host: t.String({ default: "" }),
+      user: t.String({ default: "" }),
+      password: t.String({ default: "" }),
+      label: t.String({ default: "vot-backend" }),
+    }),
+  }),
+  services: t.Object({
+    mediaConverter: t.Object({
+      hostname: t.String({ default: "http://127.0.0.1:3001" }),
+      token: t.String({ default: "" }),
+      maxReqInterrupt: t.Number({ default: 120 }), // if the number of request repeats > the maxRequestInterrupt, then abort as a failure
+      convertReqInterval: t.Number({ default: 2500 }), // 24 req/min for single convert
+    }),
+    translateText: t.Object({
+      hostname: t.String({ default: "http://127.0.0.1:3313" }),
+    }),
+  }),
+  db: t.Object({
+    name: t.String({ default: "vot-backend" }),
+    host: t.String({ default: "127.0.0.1" }),
+    port: t.Number({ default: 5432 }),
+    user: t.String({ default: "postgres" }),
+    password: t.String({ default: "postgres" }),
+    outdateAfter: t.Number({ default: 604_800_000 }), // in ms
+  }),
+  redis: t.Object({
+    host: t.String({ default: "127.0.0.1" }),
+    port: t.Number({ default: 6379 }),
+    username: t.String({ default: "default" }),
+    password: t.String({ default: "" }),
+    prefix: t.String({ default: "votb" }), // Only for DB caching. BullMQ uses other prefix!
+    ttl: t.Number({ default: 7200 }), // Only for DB caching. BullMQ uses own impl
+  }),
+  s3: t.Object({
+    region: t.String({ default: "ru-central1" }), // if you use other s3 provider change this region
+    endpoint: t.String({ default: "https://storage.yandexcloud.net" }),
+    bucket: t.String({ default: "example" }),
+    accessKeyID: t.String({ default: "" }),
+    secretAccessKey: t.String({ default: "" }),
+  }),
+  navigation: t.Object({
+    defaultLimit: t.Number({ default: 10 }),
+    maxLimit: t.Number({ default: 50 }),
+    maxPage: t.Number({ default: 2147483647 }),
+  }),
+  downloaders: t.Object({
+    userAgent: t.String({
+      default:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36",
+    }),
+  }),
+});
+
+export type ConfigSchemaType = Static<typeof ConfigSchema>;
