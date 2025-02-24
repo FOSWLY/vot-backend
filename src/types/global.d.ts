7eca3f63be68fa0ba1543ad393da7744b91704aa
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-25 00:42:52 +0300

    chore: added env variables for new config fields

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -20,9 +20,12 @@ declare module "bun" {
     REDIS_PASSWORD: string;
     S3_REGION: string;
     S3_ENDPOINT: string;
+    S3_PRESIGNED_ENDPOINT: string;
     S3_BUCKET: string;
     S3_ACCESS_KEY_ID: string;
     S3_SECRET_ACCESS_KEY: string;
+    S3_FORCE_PATH_STYLE: string;
+    LOG_TO_FILE: string;
     LOKI_HOST: string;
     LOKI_USER: string;
     LOKI_PASSWORD: string;
