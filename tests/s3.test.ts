4d64b2851888fc71fdfa4ecd0754897b83de050e
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-25 00:44:34 +0300

    test: renamed unified s3 methods

--- tests/s3.test.ts
+++ tests/s3.test.ts
@@ -1,9 +1,13 @@
 import { expect, test } from "bun:test";
 import TranslationJob from "../src/jobs/translation";
-import { deleteAudio, generatePreSigned } from "../src/s3/actions";
+import { deleteFile, generatePreSigned } from "../src/s3/actions";
 
 test("Audio upload", async () => {
-  const res = await TranslationJob.uploadTranslatedAudio("https://s3.toil.cc/test.mp3", "test");
+  const res = await TranslationJob.uploadFile(
+    "https://s3.toil.cc/test.mp3",
+    "test",
+    TranslationJob.s3AudioPrefix,
+  );
   expect(res).not.toEqual(null);
 });
 
@@ -14,7 +18,7 @@ test("Get pre signed", async () => {
 });
 
 test("Audio delete", async () => {
-  const res = await deleteAudio("vtrans/test/ae9b4d2f-abb2-421a-b2b3-4caf8fd0ed5f.mp3");
+  const res = await deleteFile("vtrans/test/ae9b4d2f-abb2-421a-b2b3-4caf8fd0ed5f.mp3");
   console.log(res);
   expect(res).not.toEqual(null);
 });
