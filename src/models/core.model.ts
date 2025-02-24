d2c7ce610fe4d2c769581e5299cd24833edc6608
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-25 00:40:56 +0300

    refactor: moved navigation params from translation

--- src/models/core.model.ts
+++ src/models/core.model.ts
@@ -17,8 +17,13 @@ export const Navigation = t.Object({
   next: t.Nullable(Page),
   prev: t.Nullable(Page),
 });
-
 export type Navigation = Static<typeof Navigation>;
+
+export const NavigationParams = t.Object({
+  // min/max validation for number doesn't work (???)
+  page: t.Optional(t.Number()),
+  limit: t.Optional(t.Number()),
+});
 
 export const coreModels = new Elysia().model({
   "not-authenticated": t.Object({
