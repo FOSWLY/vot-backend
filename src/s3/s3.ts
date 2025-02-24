1c18a244a4640447fb17d8f6529c9a3e4e0b0781
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-25 00:36:56 +0300

    chore: added separated presigned client

--- src/s3/s3.ts
+++ src/s3/s3.ts
@@ -1,9 +1,9 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
 import { FetchHttpHandler } from "@smithy/fetch-http-handler";
 
 import config from "@/config";
 
-export default new S3Client({
+const clientOpts: S3ClientConfig = {
   region: config.s3.region,
   endpoint: config.s3.endpoint,
   credentials: {
@@ -13,4 +13,11 @@ export default new S3Client({
   requestHandler: new FetchHttpHandler({
     requestTimeout: 120_000, // in ms
   }),
+  forcePathStyle: config.s3.forcePathStyle,
+};
+
+export default new S3Client(clientOpts);
+export const s3PreSignedClient = new S3Client({
+  ...clientOpts,
+  endpoint: config.s3.preSignedEndpoint,
 });
